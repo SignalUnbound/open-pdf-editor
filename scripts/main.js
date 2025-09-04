2c6f60cc81699d9218145b9f2f9eeea3e879cb1a
SignalUnbound <signal.unbound0@gmail.com>
2025-09-03 20:33:42 -0400

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// scripts/main.js
+
+console.log("🚀 Signal:Unbound PDF Editor initialized");
+
+// You’ll import each module here as we build:
+import './core/loadPDF.js';
+import './core/renderPDF.js';
+import './core/savePDF.js';
+
+import './features/addText.js';
+import './features/addImage.js';
+import './features/addPage.js';
+import './features/deletePage.js';
