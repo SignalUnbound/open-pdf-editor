20460adbcca268cb2950ceaea20735449853a6d8
SignalUnbound <signal.unbound0@gmail.com>
2025-09-03 21:48:54 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,6 @@
-// scripts/main.js
-
-console.log("🚀 Signal:Unbound PDF Editor initialized");
-
-// You’ll import each module here as we build:
 import './core/loadPDF.js';
-import './core/renderPDF.js';
-import './core/savePDF.js';
-
-import './features/addText.js';
-import './features/addImage.js';
-import './features/addPage.js';
-import './features/deletePage.js';
-
-// scripts/main.js
-
-console.log("🚀 Signal:Unbound PDF Editor initialized");
-
-// Import modules
-import './core/loadPDF.js';
-import './core/renderPDF.js';
-import './core/savePDF.js';
-
-import './features/addText.js';
-import './features/addImage.js';
-import './features/addPage.js';
-import './features/deletePage.js';
-
-// Wire up navigation buttons
-import { state } from './state.js';
 import { renderPage } from './core/renderPDF.js';
+import { state } from './state.js';
 
 document.getElementById('prev-page').addEventListener('click', () => {
   if (state.currentPage > 1) {
